539e110c7ace024be3d3d30e3d60adc9d9a2fecd
linyang <linyangflash@163.com>
2022-06-02 20:11:10 +0800

    native mode finish:

--- web/service/project/online.go
+++ web/service/project/online.go
@@ -13,13 +13,17 @@ func Online(c *gin.Context) (history *models.History, err error) {
 	if err = ginutil.ShouldBind(c, &val); err != nil {
 		return
 	}
-	if err = val.Validator(); err != nil {
-		return
-	}
+
+	var project = models.GetProject(c)
+	val.ProjectId = project.ID
+	val.Project = project
 	val.ID = 0
 	val.Uid = models.UID(c)
 	val.Status = models.HistoryStatusDefault
 
+	if err = val.Validator(); err != nil {
+		return
+	}
 	if err = models.DB().Transaction(func(tx *gorm.DB) error {
 		if er := tx.Save(&val).Error; er != nil {
 			return er
