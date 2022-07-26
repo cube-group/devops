1e8e241ba9a61659f1777c75dac506fb02330886
linyang <linyangflash@163.com>
2022-07-26 14:31:23 +0800

    v1.2.5

--- web/service/tty/create.go
+++ web/service/tty/create.go
@@ -2,6 +2,7 @@ package tty
 
 import (
 	"app/library/ginutil"
+	"app/library/types/convert"
 	"app/models"
 	"errors"
 	"fmt"
@@ -11,17 +12,19 @@ import (
 type TTYCode string
 
 const (
-	TTYCodeBash = "bash" //bash
-	TTYCodeNode = "node" //node ssh
-	TTYCodeLogs = "logs" //docker logs
-	TTYCodeExec = "exec" //docker exec
-	TTYCodeTail = "tail" //history apply tail
+	TTYCodeBash              = "bash"          //bash
+	TTYCodeNode              = "node"          //node ssh
+	TTYCodeContainerLogs     = "containerLogs" //project node container stdout
+	TTYCodeContainerExec     = "containerExec" //project node container exec
+	TTYCodeHistoryTail       = "historyTail"   //history apply tail
+	TTYCodeNodeContaienrExec = "nodeContainerExec"
+	TTYCodeNodeContaienrLogs  = "nodeContainerLogs"
 )
 
 type valCreate struct {
 	Code TTYCode `form:"code" binding:"required"`
 	ID   uint32  `form:"id" binding:"omitempty"`
-	Pod  uint32  `form:"pod" binding:"omitempty"` //for pod exec/log
+	Pod  string  `form:"pod" binding:"omitempty"` //for pod exec/log
 }
 
 //create gotty process
@@ -32,34 +35,15 @@ func Create(c *gin.Context) (res gin.H, err error) {
 		return
 	}
 	var port uint32
+	var args []string
 	switch val.Code {
-	case TTYCodeBash:
-		port, err = models.CreateGoTTY(c, true, "bash")
-		//default: "--close-signal", "1", // SIGHUP
-	case TTYCodeNode: //node
-		//permission check
-		if !models.GetUser(c).IsAdm() {
-			err = errors.New("只有管理员可操作性")
-			return
-		}
-		if node := models.GetNode(val.ID); node != nil {
-			var args []string
-			args, err = node.RunSshArgs(true, "", "")
-			if err != nil {
-				return
-			}
-			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
-			//"--close-signal", "9", // SIGKILL, kill -9
-		}
-	case TTYCodeExec: //docker exec
+	case TTYCodeContainerExec: //docker container exec
 		if h := models.GetHistory(val.ID); h != nil {
-			//permission check
 			if models.GetUser(c).HasPermissionProject(h.ProjectId) != nil {
 				err = errors.New("没有权限操作")
 				return
 			}
-			var args []string
-			if node, ok := h.Nodes.Get(val.Pod); ok {
+			if node, ok := h.Nodes.Get(convert.MustUint32(val.Pod)); ok {
 				args, err = node.RunSshArgs(true, "", fmt.Sprintf("docker exec -it %s sh", h.Project.Name))
 			} else {
 				err = errors.New("pod not found")
@@ -70,15 +54,13 @@ func Create(c *gin.Context) (res gin.H, err error) {
 			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
 			//"--close-signal", "9", // SIGKILL, kill -9
 		}
-	case TTYCodeLogs: //docker logs
+	case TTYCodeContainerLogs: //docker container logs -f
 		if h := models.GetHistory(val.ID); h != nil {
-			//permission check
 			if models.GetUser(c).HasPermissionProject(h.ProjectId) != nil {
 				err = errors.New("没有权限操作")
 				return
 			}
-			var args []string
-			if node, ok := h.Nodes.Get(val.Pod); ok {
+			if node, ok := h.Nodes.Get(convert.MustUint32(val.Pod)); ok {
 				args, err = node.RunSshArgs(false, "", fmt.Sprintf("docker logs -f -n 1000 %s", h.Project.Name))
 			} else {
 				err = errors.New("pod not found")
@@ -89,7 +71,7 @@ func Create(c *gin.Context) (res gin.H, err error) {
 			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
 			//"--close-signal", "9", // SIGKILL, kill -9
 		}
-	case TTYCodeTail: //history apply tail
+	case TTYCodeHistoryTail: //history apply tail
 		if h := models.GetHistory(val.ID); h != nil {
 			var logFilePath = h.WorkspaceFollowLog()
 			if h.IsEnd() {
@@ -103,6 +85,50 @@ func Create(c *gin.Context) (res gin.H, err error) {
 				"--close-signal", "2", // SIGINT, ctrl-c
 				"tail", "-f", "-n", "5000", logFilePath,
 			)
+		}
+	case TTYCodeBash: //system bash
+		if !models.IsAdm(c) {
+			err = errors.New("只有管理员可操作性")
+			return
+		}
+		port, err = models.CreateGoTTY(c, true, "bash")
+		//default: "--close-signal", "1", // SIGHUP
+	case TTYCodeNode: //node ssh
+		if !models.IsAdm(c) {
+			err = errors.New("只有管理员可操作性")
+			return
+		}
+		if node := models.GetNode(val.ID); node != nil {
+			args, err = node.RunSshArgs(true, "", "")
+			if err != nil {
+				return
+			}
+			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
+			//"--close-signal", "9", // SIGKILL, kill -9
+		}
+	case TTYCodeNodeContaienrExec: //node container exec
+		if !models.IsAdm(c) {
+			err = errors.New("没有权限操作")
+			return
+		}
+		if n := models.GetNode(val.ID); n != nil {
+			args, err = n.RunSshArgs(true, "", fmt.Sprintf("docker exec -it %s sh", val.Pod))
+			if err != nil {
+				return
+			}
+			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
+		}
+	case TTYCodeNodeContaienrLogs: //node container logs -f
+		if !models.IsAdm(c) {
+			err = errors.New("没有权限操作")
+			return
+		}
+		if n := models.GetNode(val.ID); n != nil {
+			args, err = n.RunSshArgs(false, "", fmt.Sprintf("docker logs -f -n 1000 %s", val.Pod))
+			if err != nil {
+				return
+			}
+			port, err = models.CreateGoTTY(c, true, append([]string{"--close-cmd", "exit"}, args...)...)
 		}
 	default:
 		return nil, errors.New("not port code " + string(val.Code))
