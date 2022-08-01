9c50f707cc5622fa2d547e5b7bf9fb4736135fbc
linyang <linyangflash@163.com>
2022-08-01 16:43:57 +0800

    cubegroup/devops-shell and cubegroup/devops-shell-java upgrade

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ build()
   docker buildx build --platform $1 -t $2 -f $3 $4 --push
 }
 
-build linux/arm64,linux/amd64 cubegroup/devops-shell:$version $path/dockerfiles/shell/Dockerfile $path/dockerfiles
-build linux/arm64,linux/amd64 cubegroup/devops-shell-java:$version $path/dockerfiles/shell-java/Dockerfile $path/dockerfiles
+build linux/arm64,linux/amd64 cubegroup/devops-shell $path/dockerfiles/shell/Dockerfile $path/dockerfiles
+build linux/arm64,linux/amd64 cubegroup/devops-shell-java $path/dockerfiles/shell-java/Dockerfile $path/dockerfiles
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/app
 build linux/arm64,linux/amd64 cubegroup/devops:$version $path/dockerfiles/devops/Dockerfile $path
